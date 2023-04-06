61cb933b3403bd6461fe43e7035edf086daf3692
Adinda Ajeng <88783221+adindaajeng@users.noreply.github.com>
2023-04-06 21:19:17 +0700

    Initial commit

---
+++ pytopg.py
@@ -0,0 +1,20 @@
+# Import library
+import psycopg2
+
+# Connect to Postgre
+# Create new database session and return a new connection object
+# The basic connection parameters including:
+# dbname - the database name
+# user - user name used to authenticate
+# password - password used to authenticate
+# host - database host address (defaults to UNIX socket if not provided)
+# port - connection port number (default to 5432 if not provided)
+
+conn = {
+    dbname = "",
+    user = "",
+    password = "",
+    host = "",
+
+}
+
